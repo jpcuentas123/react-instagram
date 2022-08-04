9cba6ca9022cdae8376b49bde2aa996dbe68368a
Jorge <jorge.perez@meltstudio.co>
2022-08-04 07:42:44 -0500

    feat: init app config

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+const App = () => {
+    useEffect(() => {
+        console.log('useEffect')
+    })
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                // test: querys for all files with .js, .jsx, .ts, .tsx extensions
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    },
+    // pass all js files through Babel
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    }
+}
